b1c9d5bb04c2c7848c5135434c592eefc2596648
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-10-14 22:30:43 +0330

    Clean Code

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+// Variable's
 const BatteryLevel = document.querySelector('button'),
     Container = document.querySelector(".container");
 
 BatteryLevel.addEventListener('click', () => {
+    // Get Battery With Navigator
     navigator.getBattery().then((battery) => {
+        // Battery Level
         let batteryLevel = battery.level * 100 + "%";
-        console.log("Your Battery Level Is ", batteryLevel);
+        // console.log("Your Battery Level Is ", batteryLevel);
+        // And Show Battery Level On Html Preview
         Container.innerHTML = `<h2 class="batteryLevel">${batteryLevel}</h2>`;
     });
 });
